12b4df9deb74a79422101207dfdbd3af85c02b69
Pascal <pascal.calixte26@gmail.com>
2019-06-22 21:46:12 -0400

    added increment counter

--- clickygame/Friendfinder/friendapp/src/App.js
+++ clickygame/Friendfinder/friendapp/src/App.js
@@ -8,14 +8,22 @@ import friends from "./friends.json";
 class App extends Component {
   // Setting this.state.friends to the friends json array
   state = {
-    friends
+    friends,
+    count: 0    
   };
 
+  
   removeFriend = id => {
     // Filter this.state.friends for friends with an id not equal to the id being removed
     const friends = this.state.friends.sort(function() {
       return 0.5 - Math.random()
     }) ;
+
+   handleIncrement = () => {
+      // We always use the setState method to update a component's state
+      this.setState({ count: this.state.count + 1 });
+    };
+
     // Set this.state.friends equal to the new friends array
     this.setState({ friends });
   };
@@ -28,7 +36,8 @@ class App extends Component {
        
         <Title>     
 
-          <Banner>Clicky Game&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span class="rest">Click an image to begin!&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Score: 0 | Top Score: 0</span></Banner>     
+          <Banner>Clicky Game&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <span class="rest">Click an image to begin!&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Score: {this.state.count} | Top Score: 0</span></Banner>     
           
           <p>Clicky Game!</p>
 
@@ -39,6 +48,7 @@ class App extends Component {
         
         {this.state.friends.map(friend => (
           <FriendCard
+            handleIncrement={this.handleIncrement}
             removeFriend={this.removeFriend}
             id={friend.id}
             key={friend.id}
--- clickygame/Friendfinder/friendapp/src/components/FriendCard/index.js
+++ clickygame/Friendfinder/friendapp/src/components/FriendCard/index.js
@@ -5,7 +5,7 @@ import "./style.css";
 function FriendCard(props) {
   return (
     <div className="card" onClick={() => props.removeFriend(props.id)}>
-      <div className="img-container">
+      <div className="img-container" onClick={() => props.handleIncrement(props.id)}>
         <img alt={props.name} src={props.image} />
       </div>
       
